f735a0399461cf5190d14fc42f4dcb5cdd226e32
GabrielMelaia231237 <231237@virtualwindow.co.za>
2023-09-03 16:54:01 +0200

    A lot of trial and error
    Tried many different things to get my filter working as well as the weather API hover

--- main.js
+++ main.js
@@ -15,13 +15,25 @@ function fetchWeather(city, infoCard) {
                 <p>Weather: ${weatherDescription}</p>
                 <p>Temperature: ${temperature}°C</p>
             `);
-            infoCard.show(); // This is to show the info card when hovering
+            infoCard.show(); // Displays the weather
         },
         error: function () {
             alert(`Failed to fetch weather data for ${city}.`);
         }
     });
 }
+
+$('.info-button').click(function () {
+    const destination = $(this).closest('.cruise-card').data('destination');
+    const infoCard = $(this).siblings('.info-card');
+
+    // Checks if weather is already fetched
+    if (infoCard.html() === '') {
+        fetchWeather(destination, infoCard);
+    } else {
+        infoCard.toggle(); // Meant to toggle on and off but doesnt work 
+    }
+});
 
 $('.info-button').on('click', function () {
     const cruiseCard = $(this).closest('.cruise-card');
@@ -30,41 +42,25 @@ $('.info-button').on('click', function () {
     fetchWeather(city, infoCard);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const cruiseCards = document.querySelectorAll(".cruise-card");
+document.getElementById('destinationFilter').addEventListener('change', function () {
+    const filterValue = this.value;
+    const cruiseCards = document.querySelectorAll('.cruise-card');
 
-    // Function to show or hide cruise cards based on filters
-    function filterCards() {
-        const durationFilter = document.getElementById("duration-filter").value;
-        const destinationFilter = document.getElementById("destination-filter").value;
-        const priceFilter = document.getElementById("price-filter").value;
+    cruiseCards.forEach(function (card) {
+        card.style.display = 'inline-block';
 
-        cruiseCards.forEach((card) => {
-            const duration = card.getAttribute("data-duration");
-            const destination = card.getAttribute("data-destination");
-            const price = parseFloat(card.getAttribute("data-price"));
-
-            const durationMatch = durationFilter === "all" || duration === durationFilter;
-            const destinationMatch = destinationFilter === "all" || destination === destinationFilter;
-            const priceMatch =
-                priceFilter === "low-to-high" || priceFilter === "high-to-low" || priceFilter === "all" || priceFilter === "custom"
-                    ? true
-                    : (priceFilter === "low-to-high" && price >= 5000 && price <= 7500) ||
-                      (priceFilter === "high-to-low" && price >= 7000 && price <= 8000);
-
-            if (durationMatch && destinationMatch && priceMatch) {
-                card.style.display = "block"; // Show the card
-            } else {
-                card.style.display = "none"; // Hide the card
-            }
-        });
-    }
-
-    // Event listeners for filter changes
-    document.getElementById("duration-filter").addEventListener("change", filterCards);
-    document.getElementById("destination-filter").addEventListener("change", filterCards);
-    document.getElementById("price-filter").addEventListener("change", filterCards);
-
-    // Initial filter
-    filterCards();
+        if (filterValue === 'short' && card.getAttribute('data-duration') !== 'short') {
+            card.style.display = 'none';
+        } else if (filterValue === 'long' && card.getAttribute('data-duration') !== 'long') {
+            card.style.display = 'none';
+        } else if (filterValue === 'single' && card.getAttribute('data-duration') !== 'single') {
+            card.style.display = 'none';
+        } else if (filterValue === 'multi' && card.getAttribute('data-duration') !== 'multi') {
+            card.style.display = 'none';
+        } else if (filterValue === 'round' && card.getAttribute('data-duration') !== 'round') {
+            card.style.display = 'none';
+        } else if (filterValue === 'cheap' && parseFloat(card.getAttribute('data-price')) > 500) {
+            card.style.display = 'none';
+        }
+    });
 });
